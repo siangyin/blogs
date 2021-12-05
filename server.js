f0daa72d421568a110c30501b876248f882eeb50
Lee Siang Yin <siangyin916@gmail.com>
2021-12-05 22:16:59 +0800

    updated css, 🎯access public dir css by app.use(express.static("public")); updated views pages and partial contents

--- server.js
+++ server.js
@@ -2,8 +2,24 @@ const express = require("express");
 const app = express();
 const PORT = process.env.PORT || 3000;
 
+// to access public dir such as css
+app.use(express.static("public"));
+
 app.get("/", (req, res) => {
 	res.render("index.ejs");
+});
+
+app.get("/about", (req, res) => {
+	res.render("about.ejs");
+});
+
+app.get("/blogs/create", (req, res) => {
+	res.render("create.ejs");
+});
+
+//404 page
+app.use((req, res) => {
+	res.status(404).render("404.ejs");
 });
 
 app.listen(PORT, () => {
