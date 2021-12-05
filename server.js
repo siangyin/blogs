353e3d3c63138fdd9ac485ae1f8ee70a34bf4985
Lee Siang Yin <siangyin916@gmail.com>
2021-12-05 22:54:48 +0800

    🕹 test data, map each entries in ejs

--- server.js
+++ server.js
@@ -5,21 +5,36 @@ const PORT = process.env.PORT || 3000;
 // to access public dir such as css
 app.use(express.static("public"));
 
+const blogs = [
+	{
+		title: "Yoshi finds eggs",
+		snippet: "Lorem ipsum dolor sit amet consectetur",
+	},
+	{
+		title: "Mario finds stars",
+		snippet: "Lorem ipsum dolor sit amet consectetur",
+	},
+	{
+		title: "How to defeat bowser",
+		snippet: "Lorem ipsum dolor sit amet consectetur",
+	},
+];
+
 app.get("/", (req, res) => {
-	res.render("index.ejs", { title: "Home" });
+	res.render("index.ejs", { title: "Home", blogs });
 });
 
 app.get("/about", (req, res) => {
-	res.render("about.ejs");
+	res.render("about.ejs", { title: "About" });
 });
 
 app.get("/blogs/create", (req, res) => {
-	res.render("create.ejs");
+	res.render("create.ejs", { title: "Create a new blog" });
 });
 
 //404 page
 app.use((req, res) => {
-	res.status(404).render("404.ejs");
+	res.status(404).render("404.ejs", { title: "404" });
 });
 
 app.listen(PORT, () => {
